4c64475b55be146b5ba54eeb217024dcfe95199b
guido.docter <guido.docter@gmail.om>
2025-09-23 21:49:44 +0200

    deploy to github pages

--- iptv-player/vite.config.ts
+++ iptv-player/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/iptv-webplayer/',
   build: {
     rollupOptions: {
       // Don't externalize react-dom for the application build
